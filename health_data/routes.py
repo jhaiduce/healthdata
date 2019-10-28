72250082c24544c71d0a6dd5df6afda0bb81da5c
John Haiducek <jhaiduce@picard.local>
2019-10-28 19:24:45 -0400

    Added missing config for deform static files

--- health_data/routes.py
+++ health_data/routes.py
@@ -1,4 +1,5 @@
 def includeme(config):
+    config.add_static_view('deform_static', 'deform:static/')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('period_add','/period/add')
     config.add_route('home', '/')
