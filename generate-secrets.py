ea50292291693693df17520336b7f67b4f822c1c
John Haiducek <jhaiduce@picard.local>
2020-03-10 21:10:03 -0400

    Moved secret generation into a function, added an option to change the directory where secrets are written, and generate an ini file with passwords included

--- generate-secrets.py
+++ generate-secrets.py
@@ -3,49 +3,65 @@ import os
 import random
 import string
 
-if not os.path.exists('secrets'):
-    os.mkdir('secrets')
+def generate_secrets(secrets_dir='secrets',ini_template='production.ini.tpl',iniout='production.ini'):
+    if not os.path.exists(secrets_dir):
+        os.mkdir(secrets_dir)
 
-if not (os.path.exists('secrets/ca-key.pem') or os.path.exists('secrets/ca.pem')):
-    print('Generating root certificate')
-    call(['openssl','genrsa','2048'],stdout=open('secrets/ca-key.pem','w'))
-    call(['openssl','req','-new','-x509','-nodes','-days','365000',
-          '-key','secrets/ca-key.pem','-out','secrets/ca.pem'])
+    if not (os.path.exists(secrets_dir+'/ca-key.pem') or os.path.exists(secrets_dir+'/ca.pem')):
+        print('Generating root certificate')
+        call(['openssl','genrsa','2048'],stdout=open(secrets_dir+'/ca-key.pem','w'))
+        call(['openssl','req','-new','-x509','-nodes','-days','365000',
+              '-key',secrets_dir+'/ca-key.pem','-out',secrets_dir+'/ca.pem'])
 
-if not (os.path.exists('secrets/server-key.pem') or os.path.exists('secrets/server-req.pem')):
+    if not (os.path.exists(secrets_dir+'/server-key.pem') or os.path.exists(secrets_dir+'/server-req.pem')):
 
-    print('Generating server key')
-    call(['openssl','req','-newkey','rsa:2048','-days','365000','-nodes',
-          '-keyout','secrets/server-key.pem','-out','secrets/server-req.pem'])
-    call(['openssl','rsa','-in','secrets/server-key.pem',
-          '-out','secrets/server-key.pem'])
-        
-if not os.path.exists('secrets/server-cert.pem'):
+        print('Generating server key')
+        call(['openssl','req','-newkey','rsa:2048','-days','365000','-nodes',
+              '-keyout',secrets_dir+'/server-key.pem','-out',secrets_dir+'/server-req.pem'])
+        call(['openssl','rsa','-in',secrets_dir+'/server-key.pem',
+              '-out',secrets_dir+'/server-key.pem'])
 
-    print('Generating server certificate')
-    call(['openssl','x509','-req','-in','secrets/server-req.pem',
-          '-days','365000','-CA','secrets/ca.pem',
-          '-CAkey','secrets/ca-key.pem','-set_serial','01',
-          '-out','secrets/server-cert.pem'])
+    if not os.path.exists(secrets_dir+'/server-cert.pem'):
 
-call(['openssl','verify','-CAfile','secrets/ca.pem','secrets/server-cert.pem'])
+        print('Generating server certificate')
+        call(['openssl','x509','-req','-in',secrets_dir+'/server-req.pem',
+              '-days','365000','-CA',secrets_dir+'/ca.pem',
+              '-CAkey',secrets_dir+'/ca-key.pem','-set_serial','01',
+              '-out',secrets_dir+'/server-cert.pem'])
 
-if not os.path.exists('secrets/storage_key.keyfile'):
+    call(['openssl','verify','-CAfile',secrets_dir+'/ca.pem',secrets_dir+'/server-cert.pem'])
 
-    # Generate key for MariaDB at-rest encryption
-    storage_key=open('secrets/storage_key.keyfile','w')
-    storage_key.write('1;')
-    storage_key.flush()
-    call(['openssl','rand','-hex','32'],stdout=storage_key)
+    if not os.path.exists(secrets_dir+'/storage_key.keyfile'):
 
-def genPassword(length=24,charset=string.printable):
-    return ''.join([random.choice(charset) for i in range(length)])
+        # Generate key for MariaDB at-rest encryption
+        storage_key=open(secrets_dir+'/storage_key.keyfile','w')
+        storage_key.write('1;')
+        storage_key.flush()
+        call(['openssl','rand','-hex','32'],stdout=storage_key)
 
-def write_password(filename,overwrite=False,*args,**kwargs):
-    if not os.path.exists(filename) or overwrite:
-        open(filename,'w').write(genPassword(*args,**kwargs))
+    def genPassword(length=24,charset=string.printable):
+        return ''.join([random.choice(charset) for i in range(length)])
 
-write_password('secrets/db_root_pw')
-write_password('secrets/db_app_pw')
-write_password('secrets/app_admin_pw')
-write_password('secrets/pyramid_auth_secret')
+    def write_password(filename,overwrite=False,*args,**kwargs):
+        if not os.path.exists(filename) or overwrite:
+            pw=genPassword(*args,**kwargs)
+            open(filename,'w').write(pw)
+        else:
+            pw=open(filename).read()
+        return pw
+
+    db_root_pw=write_password(secrets_dir+'/db_root_pw')
+    db_app_pw=write_password(secrets_dir+'/db_app_pw')
+    app_admin_pw=write_password(secrets_dir+'/app_admin_pw')
+    pyramid_auth_secret=write_password(secrets_dir+'/pyramid_auth_secret')
+
+    ini_text=open(ini_template).read().format(
+        mysql_production_password=db_app_pw,
+        mysql_root_password=db_root_pw,
+        app_admin_password=app_admin_pw,
+        pyramid_auth_secret=pyramid_auth_secret
+    )
+    open(os.path.join(secrets_dir,iniout),'w').write(ini_text)
+
+if __name__=='__main__':
+    generate_secrets('integration_test_secrets',ini_template='integration_test.ini.tpl',iniout='integration_test.ini')
