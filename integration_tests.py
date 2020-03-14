a613c3dff9c97c517c596c0a763be1bd7c36aa51
John Haiducek <jhaiduce@picard.local>
2020-03-14 17:04:25 -0400

    Added a test of the period entry form

--- integration_tests.py
+++ integration_tests.py
@@ -47,3 +47,32 @@ class BaseTest(unittest.TestCase):
     def test_period_plot(self):
         resp=self.session.get('http://healthdata_web/period')
         self.assertGreater(resp.text.find('<div class="bk-root"'),0)
+
+    def test_period_add(self):
+        resp=self.session.get('http://healthdata_web/period/list')
+        entry_count=int(re.search(r'(\d+) entries',resp.text).group(1))
+
+        resp=self.session.post(
+            'http://healthdata_web/period/add',
+            data=[
+                ('__start__','date:mapping'),
+                ('date','2020-03-14'),
+                ('__end__','date:mapping'),
+                ('__start__','temperature_time:mapping'),
+                ('time','07:30'),
+                ('__end__','temperature_time:mapping'),
+                ('temperature','97.9'),
+                ('period_intensity','1'),
+                ('cervical_fluid','1'),
+                ('submit','submit')
+            ]
+        )
+
+        # Check that we got redirected
+        self.assertEqual(resp.history[0].status_code,302)
+
+        # Check that equipment was added to the list
+        resp=self.session.get('http://healthdata_web/period/list')
+        new_entry_count=int(re.search(r'(\d+) entries',resp.text).group(1))
+
+        self.assertEqual(new_entry_count,entry_count+1)
