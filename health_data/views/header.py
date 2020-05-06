e6fdfe92a732392734a3c18be6a7fd867f8c002a
John Haiducek <jhaiduce@picard.local>
2020-05-05 23:08:38 -0400

    User request.authenticated_userid instead of pyramid.security.authenticated_userid

--- health_data/views/header.py
+++ health_data/views/header.py
@@ -1,6 +1,5 @@
 from ..models import User
 from ..models import Person
-from pyramid.security import authenticated_userid
 
 def view_with_header(view_callable):
 
@@ -14,10 +13,10 @@ def view_with_header(view_callable):
         # Turn on the header
         data['show_header']=True
 
-        if authenticated_userid(request):
+        if request.authenticated_userid is not None:
             # Get the user information
             user=request.dbsession.query(User).filter(
-                User.id==authenticated_userid(request)).one()
+                User.id==request.authenticated_userid).one()
             data['username']=user.name
             data['userid']=user.id
             data['request_url']=request.url
