2a2e320c0b5ff15a155cdbc0aa7057c6f6092a1e
John Haiducek <jhaiduce@gmail.com>
2021-01-14 10:10:00 -0500

    Add sqlalchemy-diff to testing requirements

--- setup.py
+++ setup.py
@@ -34,7 +34,8 @@ tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest >= 3.7.4',
     'pytest-cov',
-    'alembic-verify'
+    'alembic-verify',
+    'sqlalchemy-diff == 0.1.3'
 ]
 
 setup(
