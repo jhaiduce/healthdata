9f38082821d1e7210043eed23ee91d75034adfa9
John Haiducek <jhaiduce@gmail.com>
2021-01-23 17:32:35 -0500

    Register custom math and date functions in sqlite

--- health_data/models/records.py
+++ health_data/models/records.py
@@ -27,6 +27,43 @@ from .meta import Base
 
 from datetime import datetime, time, timedelta
 
+def subtime(datetime_str,time_str):
+    from sqlalchemy.dialects import sqlite
+
+    if datetime_str is None or time_str is None:
+        return None
+
+    inp_datetime=sqlite.DATETIME().result_processor(sqlite,sqlite.DATETIME())(datetime_str)
+    inp_time=sqlite.TIME().result_processor(sqlite,sqlite.TIME())(time_str)
+
+    result = inp_datetime - (datetime.combine(datetime.min,inp_time) - datetime.combine(datetime.min,time(0)))
+
+    result_str=sqlite.DATETIME().bind_processor(sqlite)(result)
+
+    return result_str
+
+def get_time_from_datetime(datetime_str):
+    from sqlalchemy.dialects import sqlite
+
+    if datetime_str is None: return None
+
+    inp_datetime=sqlite.DATETIME().result_processor(sqlite,sqlite.DATETIME())(datetime_str)
+    result=inp_datetime.time()
+    result_str=sqlite.TIME().bind_processor(sqlite)(result)
+    return result_str
+
+@sa.event.listens_for(sa.engine.Engine,'connect')
+def register_functions(conn, connection_record):
+    from sqlite3 import Connection as sqliteConnection
+    import math
+    if isinstance(conn,sqliteConnection):
+        conn.create_function("cos", 1, math.cos)
+        conn.create_function("sin", 1, math.sin)
+        conn.create_function("acos", 1, math.acos)
+        conn.create_function("atan2", 2, math.atan2)
+        conn.create_function("subtime", 2, subtime)
+        conn.create_function("time", 1, get_time_from_datetime)
+
 class Record(Base):
 
     __tablename__='record'
