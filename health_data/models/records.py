3e7f4a3f45a203bdc553f20b834b97dc3f2f096a
John Haiducek <jhaiduce@gmail.com>
2021-01-24 19:57:55 -0500

    Added time_to_sec function for use in sqlite

--- health_data/models/records.py
+++ health_data/models/records.py
@@ -52,6 +52,15 @@ def get_time_from_datetime(datetime_str):
     result_str=sqlite.TIME().bind_processor(sqlite)(result)
     return result_str
 
+def time_to_sec(time_str):
+    from sqlalchemy.dialects import sqlite
+
+    if time_str is None: return None
+
+    inp_time=sqlite.TIME().result_processor(sqlite,sqlite.TIME())(time_str)
+
+    return (inp_time-time(0)).total_seconds()
+
 @sa.event.listens_for(sa.engine.Engine,'connect')
 def register_functions(conn, connection_record):
     from sqlite3 import Connection as sqliteConnection
@@ -63,6 +72,7 @@ def register_functions(conn, connection_record):
         conn.create_function("atan2", 2, math.atan2)
         conn.create_function("subtime", 2, subtime)
         conn.create_function("time", 1, get_time_from_datetime)
+        conn.create_function("time_to_sec", 1, time_to_sec)
 
 class Record(Base):
 
