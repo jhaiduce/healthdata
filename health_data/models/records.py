e78e942eaaf1b588a4f6581d6b065c14b5c7f219
John Haiducek <jhaiduce@gmail.com>
2021-01-24 19:58:21 -0500

    Fixes to hybrid_property expressions used for computing menstrual flow rates

--- health_data/models/records.py
+++ health_data/models/records.py
@@ -268,14 +268,14 @@ class MenstrualCupFill(TimestampedRecord,IndividualRecord,Record):
         previous=aliased(cls)
 
         last_removal_time=sa.select([
-            func.max(previous.removal_time,func.to_date(cls.removal_time)-1)
+            func.max(previous.removal_time,func.subtime(cls.removal_time,func.time(cls.removal_time))-1)
         ]).where(
             previous.removal_time<cls.removal_time
         ).order_by(previous.removal_time.desc()).limit(1).as_scalar()
 
         insertion_time=case(
             [
-                (cls.insertion_time_==None,func.max(last_removal_time,func.to_date(cls.removal_time))),
+                (cls.insertion_time_==None,func.max(last_removal_time,func.subtime(cls.removal_time,func.time(cls.removal_time)))),
             ], else_ = cls.insertion_time_
         )
 
@@ -295,10 +295,10 @@ class MenstrualCupFill(TimestampedRecord,IndividualRecord,Record):
         return self.fill/hours
 
     @flow_rate.expression
-    def flow_rate(self):
+    def flow_rate(cls):
 
         return func.time_to_sec(
-            func.timediff(cls.removal_time,cls.insertion_time))
+            func.subtime(cls.removal_time,cls.insertion_time))
 
     __mapper_args__ = {
         'polymorphic_identity':'menstrual_cup_fill'
