1a3907f1afa5b5da948c9536252ef95b64feb1ac
John Haiducek <jhaiduce@gmail.com>
2021-05-03 22:53:36 -0400

    Fixes to handling of exit codes from Docker containers

--- migrate_db_ci.sh
+++ migrate_db_ci.sh
@@ -5,7 +5,9 @@ set -e
 # Migrate database
 sudo docker-compose -f docker-compose.test_secrets.yml -f docker-compose.db.yml -f docker-compose.migrate.yml -p healthdata_ci up -d
 
-sudo docker wait healthdata_ci_migration_1
+exitcode=$(sudo docker wait healthdata_ci_migration_1)
 
 # Print migration logs
 sudo docker logs healthdata_ci_migration_1
+
+exit $exitcode
